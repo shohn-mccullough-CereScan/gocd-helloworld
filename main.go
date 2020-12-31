1adfaf769359786f9ea1312fcc6457aa98d10845
shohn-mccullough-CereScan <73607533+shohn-mccullough-CereScan@users.noreply.github.com>
2020-12-31 15:36:17 -0700

    Update main.go
    Adding some error states and argument flags.

--- main.go
+++ main.go
@@ -1,9 +1,29 @@
-package main
-
 import(
+  "flag"
   "fmt"
 )
 
-func main() {
+func mainTest() {
+  var err error = nil
   fmt.Println("Hello World!!")
+  
+  var flagVal string
+  flag.StringVar(&flagVal, "args")
+  
+  flag.Parse()
+  
+  str, err := testCLI(flagVal)
+  
+  if err != nil {
+    log.Panic(err)
+  }
+}
+
+func testCLI(arg string) (string, error) {
+  if len(arg) < 1 {
+    err := errors.New("The argument length is less than 1")
+    return "", err
+  }
+  
+  return arg, nil
 }
